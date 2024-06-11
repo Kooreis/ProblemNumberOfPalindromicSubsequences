5dc779367420a1d3435ce063c724fd58e909807a
Kooreis <kooreis.official@gmail.com>
2024-06-10 19:55:49 -0500

    First loop to fill the 'prev' array with the previous index of each character in the string.

--- solution.cs
+++ solution.cs
@@ -1,8 +1,10 @@
-function countPalindromicSubsequences(str) {
-    const n = str.length;
-    const mod = Math.pow(10, 9) + 7;
-    const dp = Array.from({ length: n }, () => Array(n).fill(0));
-    const next = Array.from({ length: n }, () => Array(4).fill(0));
-    const prev = Array.from({ length: n }, () => Array(4).fill(0));
-    const aCharCode = 'a'.charCodeAt(0);
-}
+for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < 4; ++j) {
+            if (i == 0) {
+                prev[i][j] = -1;
+            } else {
+                prev[i][j] = prev[i - 1][j];
+            }
+        }
+        prev[i][str[i].charCodeAt(0) - aCharCode] = i;
+    }
